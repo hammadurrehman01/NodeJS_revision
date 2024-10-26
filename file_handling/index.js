de235c6a5f01337310d830662515434d609ce31e
hammadurrehman01 <hammadurrehman@techlabs.cc>
2024-10-26 10:47:54 +0500

    working with file system

---
+++ file_handling/index.js
@@ -0,0 +1,32 @@
+import fs from "fs";
+
+// Writing File ....
+// Sync
+// fs.writeFileSync("./text.txt", "Hello Hammad This is sync file");
+// console.log("code ran");
+
+
+// Async
+// fs.writeFile("./async.txt", "Hello Hammad This is async file", (err, data) => {
+//     if (err) {
+//         console.log(err)
+//     }
+// })
+
+
+
+
+
+
+// ====================================================== //
+// Reading file
+
+// const result = fs.readFileSync("./text.txt", "utf-8");
+// console.log(result);
+
+
+
+// =========================================================== //
+
+// fs.appendFileSync("text.txt", "Hello Hammad This is sync file\n");
+// console.log(fs.statSync("text.txt"));
